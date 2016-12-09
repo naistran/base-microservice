0bd4c15b8e44e2d59419cc4c928112a722f45bcd
Nathan Tran <ntran013@gmail.com>
2016-12-09 17:28:47 +1100

    Use micro-scripts

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 // @flow
 
-import micro from 'micro';
 import type { ClientRequest, ServerResponse } from 'http';
 
-const srv = micro(async (req: ClientRequest, res: ServerResponse) => {
-  res.end('hehehehehehehe');
-});
-
-const { PORT = 3000 } = process.env;
-srv.listen(PORT);
+export default async (req: ClientRequest, res: ServerResponse) => {
+  res.end('Hello, world.');
+};
